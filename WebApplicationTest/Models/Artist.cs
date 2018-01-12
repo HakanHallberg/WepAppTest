e4ff6235f09f71becd2636e2f8093817b9f239ed
Håkan Hallberg <hakan.hallberg.spo16@ecusolna.onmicrosoft.com>
2018-01-12 10:41:13 +0100

    Added entitys Artist and Song

---
+++ WebApplicationTest/Models/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationTest.Models
+{
+    public class Artist
+    {
+        public int ArtistID { get; set; }
+        public string ArtistName { get; set; }
+    }
+}
