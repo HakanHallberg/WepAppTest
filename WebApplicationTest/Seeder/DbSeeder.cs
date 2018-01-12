b25305e5f160df48d9aa712abebfa5872a2cc16b
Håkan Hallberg <hakan.hallberg.spo16@ecusolna.onmicrosoft.com>
2018-01-12 09:49:31 +0100

    Created DbSeeder with user and role seed

--- WebApplicationTest/Seeder/DbSeeder.cs
+++ WebApplicationTest/Seeder/DbSeeder.cs
@@ -1,15 +1,22 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplicationTest.Data;
+using WebApplicationTest.Models;
 
 namespace WebApplicationTest.Seeder
 {
     public class DbSeeder
     {
-        public static void Seed()
+        public static void Seed(ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
-
+            var User = new ApplicationUser { UserName = "admin" };
+            var result = userManager.CreateAsync(User, "12345").Result;
+            var Admin = new IdentityRole { Name = "admin" };
+            var role = roleManager.CreateAsync(Admin).Result;
+            userManager.AddToRoleAsync(User, "admin");
         }
     }
 }
